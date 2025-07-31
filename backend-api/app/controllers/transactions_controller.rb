87730d39c841b13570b21aaeb77afeea803f51ce
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2025-07-31 19:09:43 +0800

    Added /get and /post to transactions_controller. Enabled CORS

--- backend-api/app/controllers/transactions_controller.rb
+++ backend-api/app/controllers/transactions_controller.rb
@@ -1,2 +1,28 @@
 class TransactionsController < ApplicationController
+  def index
+    transactions = TransactionCsvService.all
+    render json: transactions
+  end
+
+  def create
+    status = ["Pending", "Settled", "Failed"].sample
+
+    transaction = {
+      "Transaction Date" => transaction_params[:transaction_date],
+      "Account Number" => transaction_params[:account_number],
+      "Account Holder Name" => transaction_params[:account_holder_name],
+      "Amount" => transaction_params[:amount],
+      "Status" => status
+    }
+
+    TransactionCsvService.add(transaction)
+
+    render json: transaction, status: :created
+  end
+
+  private
+
+  def transaction_params
+    params.permit(:transaction_date, :account_number, :account_holder_name, :amount)
+  end
 end
